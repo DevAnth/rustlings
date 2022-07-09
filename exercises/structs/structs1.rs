7da8fc55b78480ff90913fb725bec88228610439
devanth <deevanth@gmail.com>
2022-07-09 12:51:22 +0200

    testing

--- exercises/structs/structs1.rs
+++ exercises/structs/structs1.rs
@@ -5,12 +5,21 @@
 
 struct ColorClassicStruct {
     // TODO: Something goes here
+    name : String,
+    hex : String
 }
 
-struct ColorTupleStruct(/* TODO: Something goes here */);
+struct ColorTupleStruct(String, String);
 
 #[derive(Debug)]
 struct UnitStruct;
+
+impl UnitStruct
+{
+    pub fn to_string() -> String{
+        "test".to_string()
+    }
+}
 
 #[cfg(test)]
 mod tests {
@@ -19,8 +28,12 @@ mod tests {
     #[test]
     fn classic_c_structs() {
         // TODO: Instantiate a classic c struct!
-        // let green =
-
+        let green = ColorClassicStruct
+        {
+            name : "green".to_string(), 
+            hex: "#00FF00".to_string(),
+        };
+        
         assert_eq!(green.name, "green");
         assert_eq!(green.hex, "#00FF00");
     }
@@ -28,7 +41,7 @@ mod tests {
     #[test]
     fn tuple_structs() {
         // TODO: Instantiate a tuple struct!
-        // let green =
+        let green = ColorTupleStruct("green".to_string(), "#00FF00".to_string());
 
         assert_eq!(green.0, "green");
         assert_eq!(green.1, "#00FF00");
@@ -37,7 +50,7 @@ mod tests {
     #[test]
     fn unit_structs() {
         // TODO: Instantiate a unit struct!
-        // let unit_struct =
+        let unit_struct = UnitStruct {};
         let message = format!("{:?}s are fun!", unit_struct);
 
         assert_eq!(message, "UnitStructs are fun!");
