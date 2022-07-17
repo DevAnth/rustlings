91d96d8d8aa578155620b7dca08d489c2508dbe4
devanth <deevanth@gmail.com>
2022-07-17 18:22:55 +0200

    errors_handling

--- exercises/error_handling/errors3.rs
+++ exercises/error_handling/errors3.rs
@@ -2,24 +2,45 @@
 // This is a program that is trying to use a completed version of the
 // `total_cost` function from the previous exercise. It's not working though!
 // Why not? What should we do to fix it?
-// Execute `rustlings hint errors3` for hints!
-
-// I AM NOT DONE
+// Execute `rustlings h
 
 use std::num::ParseIntError;
 
-fn main() {
+// fn main() {
+//     let mut tokens = 100;
+//     let pretend_user_input = "8";
+// 
+//     let cost = total_cost(pretend_user_input);
+// 
+//     match cost
+//     {
+//         Ok(c) =>
+//             {
+//                 if c > tokens {
+//                     println!("You can't afford that many!");
+//                 } else {
+//                     tokens -= c;
+//                     println!("You now have {} tokens.", tokens);
+//                 }
+//             },
+//         Err(msg) => println!("Couldn't parse int")
+//     };
+// }
+
+fn main() -> Result<(), ParseIntError> {
     let mut tokens = 100;
     let pretend_user_input = "8";
 
     let cost = total_cost(pretend_user_input)?;
 
-    if cost > tokens {
+    if cost > tokens {  
         println!("You can't afford that many!");
     } else {
         tokens -= cost;
         println!("You now have {} tokens.", tokens);
     }
+    
+    Ok(())
 }
 
 pub fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
