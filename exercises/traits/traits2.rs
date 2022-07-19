984019093a73495bf23634294eff428c63421d2a
devanth <deevanth@gmail.com>
2022-07-19 22:33:29 +0200

    traits

--- exercises/traits/traits2.rs
+++ exercises/traits/traits2.rs
@@ -10,13 +10,31 @@
 // No boiler plate code this time,
 // you can do this!
 
-// I AM NOT DONE
+trait AppendBarRef {
+    fn append_bar_ref(&mut self) -> &Self;
+}
 
 trait AppendBar {
     fn append_bar(self) -> Self;
 }
 
 //TODO: Add your code here
+
+impl AppendBar for Vec<String>
+{
+    fn append_bar(mut self) -> Self {
+        self.push(String::from("Bar"));
+        self
+    }
+}
+
+impl AppendBarRef for Vec<String>
+{
+    fn append_bar_ref(&mut self) -> &Self {
+        self.push(String::from("Bar"));
+        self
+    }
+}
 
 #[cfg(test)]
 mod tests {
@@ -25,6 +43,15 @@ mod tests {
     #[test]
     fn is_vec_pop_eq_bar() {
         let mut foo = vec![String::from("Foo")].append_bar();
+        assert_eq!(foo.pop().unwrap(), String::from("Bar"));
+        assert_eq!(foo.pop().unwrap(), String::from("Foo"));
+    }
+
+    #[test]
+    fn testing_by_ref() {
+        let mut foo = vec![String::from("Foo")];
+        foo.append_bar_ref();
+
         assert_eq!(foo.pop().unwrap(), String::from("Bar"));
         assert_eq!(foo.pop().unwrap(), String::from("Foo"));
     }
