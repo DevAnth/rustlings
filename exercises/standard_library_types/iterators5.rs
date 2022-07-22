de9c8c3e065b929b13c79a081cffa53c9dac4e23
devanth <deevanth@gmail.com>
2022-07-22 20:07:33 +0200

    iterators done

--- exercises/standard_library_types/iterators5.rs
+++ exercises/standard_library_types/iterators5.rs
@@ -10,7 +10,6 @@
 //
 // Make the code compile and the tests pass.
 
-// I AM NOT DONE
 
 use std::collections::HashMap;
 
@@ -34,6 +33,16 @@ fn count_for(map: &HashMap<String, Progress>, value: Progress) -> usize {
 fn count_iterator(map: &HashMap<String, Progress>, value: Progress) -> usize {
     // map is a hashmap with String keys and Progress values.
     // map = { "variables1": Complete, "from_str": None, ... }
+    //map.values().into_iter().filter(|x| x == &&value).count()
+    /*
+    map.values().into_iter().fold(0, |acc : usize, x| {
+        if x == &value {
+            return acc + 1;
+        };
+        return acc;
+    }) as usize*/
+    //map.values().into_iter().filter(|x| x == &&value).fold(0, |acc, x| acc + 1)
+    map.values().into_iter().filter(|x| x == &&value).count()
 }
 
 fn count_collection_for(collection: &[HashMap<String, Progress>], value: Progress) -> usize {
@@ -52,6 +61,8 @@ fn count_collection_iterator(collection: &[HashMap<String, Progress>], value: Pr
     // collection is a slice of hashmaps.
     // collection = [{ "variables1": Complete, "from_str": None, ... },
     //     { "variables2": Complete, ... }, ... ]
+    //collection.into_iter().map(|x| x.values().into_iter().filter(|x| x == &&value).count()).sum()
+    collection.into_iter().map(|x| count_iterator(x, value)).sum()
 }
 
 #[cfg(test)]
